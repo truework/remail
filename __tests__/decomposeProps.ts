5b6928a67ffbc9ccc9e4f66319a60ee37cecc80d
estrattonbailey <ericstrattonbailey@gmail.com>
2021-07-15 20:53:08 -0500

    chore: init

---
+++ __tests__/decomposeProps.ts
@@ -0,0 +1,114 @@
+import tap from 'tap'
+
+import * as Lib from '../'
+
+const { decomposeProps, defaultTheme } = Lib
+
+tap.test('ignores non-custom values', async t => {
+  const { attr } = decomposeProps(
+    {
+      width: '500px'
+    },
+    defaultTheme
+  )
+
+  t.ok(attr.width === '500px')
+})
+
+tap.test('values in theme', async t => {
+  const { style } = decomposeProps(
+    {
+      c: 'black',
+      p: 2
+    },
+    defaultTheme
+  )
+
+  // @ts-ignore
+  t.ok(style.color === defaultTheme.color.black)
+  t.ok(style.paddingTop === '8px')
+})
+
+tap.test('values not in theme', async t => {
+  const { style } = decomposeProps(
+    {
+      c: 'blue',
+      p: 20
+    },
+    defaultTheme
+  )
+
+  t.ok(style.color === 'blue')
+  t.ok(style.paddingTop === '20px')
+})
+
+tap.test('string numbers', async t => {
+  const { style } = decomposeProps(
+    {
+      lh: '1'
+    },
+    defaultTheme
+  )
+
+  t.ok(style.lineHeight !== '1')
+  t.ok(/[0-9]px/.test(style.lineHeight))
+})
+
+tap.test('toUnit', async t => {
+  const { style } = decomposeProps(
+    {
+      fw: 9
+    },
+    defaultTheme
+  )
+
+  t.ok(style.fontWeight === '900')
+})
+
+tap.test('decomposeProps - width int', async t => {
+  const { attr, style } = decomposeProps(
+    {
+      w: 200
+    },
+    defaultTheme
+  )
+
+  t.ok(style.width === '200px')
+  t.ok(attr.width === 200)
+})
+
+tap.test('decomposeProps - width unitless string', async t => {
+  const { attr, style } = decomposeProps(
+    {
+      w: '200'
+    },
+    defaultTheme
+  )
+
+  t.ok(style.width === '200px')
+  t.ok(attr.width === 200)
+})
+
+tap.test('decomposeProps - width string', async t => {
+  const { attr, style } = decomposeProps(
+    {
+      w: '200px'
+    },
+    defaultTheme
+  )
+
+  t.ok(style.width === '200px')
+  t.ok(attr.width === 200)
+})
+
+tap.test('decomposeProps - width percent', async t => {
+  const { attr, style } = decomposeProps(
+    {
+      w: '100%'
+    },
+    defaultTheme
+  )
+
+  t.ok(style.width === '100%')
+  t.ok(attr.width === '100%')
+})
---
+++ __tests__/getMobileCss.ts
@@ -0,0 +1,77 @@
+import tap from 'tap'
+
+import * as Lib from '../'
+
+const { defaultTheme, getMobileCss, getMobileStyleTree, decomposeProps } = Lib
+
+tap.test('mobile values are extracted', async t => {
+  const { className, style } = decomposeProps(
+    {
+      c: ['black', 'blue'],
+      p: [4, 2]
+    },
+    defaultTheme
+  )
+  const css = getMobileStyleTree()
+
+  // @ts-ignore
+  t.ok(style.color === defaultTheme.color.black)
+  t.ok(style.paddingTop === '16px')
+  t.ok(css[className][1].value === '8px')
+
+  // values were reset
+  t.ok(!Object.keys(getMobileStyleTree()).length)
+})
+
+tap.test('extracts CSS', async t => {
+  const { className, style } = decomposeProps(
+    {
+      c: ['black', 'blue'],
+      p: [4, 2]
+    },
+    defaultTheme
+  )
+
+  const css = getMobileCss()
+
+  t.ok(/color:blue/.test(css))
+  t.ok(/padding-top:8px/.test(css))
+})
+
+tap.test('getMobileCss - width values are extracted', async t => {
+  const { className, attr, style } = decomposeProps(
+    {
+      w: ['200px', '100%']
+    },
+    defaultTheme
+  )
+  const css = getMobileStyleTree()
+
+  t.ok(style.width === '200px')
+  t.ok(attr.width === 200)
+  t.ok(css[className][0].value === '100%')
+})
+
+tap.test('getMobileCss - mobile width int', async t => {
+  const { className, attr, style } = decomposeProps(
+    {
+      w: ['200px', 100]
+    },
+    defaultTheme
+  )
+  const css = getMobileStyleTree()
+
+  t.ok(css[className][0].value === '100px')
+})
+
+tap.test('getMobileCss - mobile width unitless string', async t => {
+  const { className, attr, style } = decomposeProps(
+    {
+      w: ['200px', '100']
+    },
+    defaultTheme
+  )
+  const css = getMobileStyleTree()
+
+  t.ok(css[className][0].value === '100px')
+})
